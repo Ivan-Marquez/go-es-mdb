d289ac2a4c012a1669c4c715cc3a678d340df59a
Ivan Marquez <js.ivan.marquez@gmail.com>
2020-02-26 14:16:02 -0400

    REF: esImport module refactor
    divide code into separate functions.

--- tools/mockData/esImport.go
+++ tools/mockData/esImport.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"sync"
 
+	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/elastic/go-elasticsearch/v8/esapi"
 	"github.com/ivan-marquez/es-mdb/pkg/storage"
 	"go.mongodb.org/mongo-driver/bson"
@@ -75,43 +76,48 @@ func importUsersDataES(store *storage.Storage) error {
 	for i, user := range users {
 		wg.Add(1)
 
-		go func(i int, u *User) error {
+		go func(i int, u *User) {
 			defer wg.Done()
-
-			// Build the request body.
-			body, _ := json.Marshal(u)
-
-			// Set up the request object.
-			req := esapi.IndexRequest{
-				Index:      "users",
-				DocumentID: u.ID.Hex(),
-				Body:       bytes.NewReader(body),
-				Refresh:    "true",
-			}
-
-			// Perform the request with the client.
-			res, err := req.Do(context.Background(), es)
-			if err != nil {
-				return fmt.Errorf("Error getting response: %s", err)
-			}
-			defer res.Body.Close()
-
-			if res.IsError() {
-				log.Printf("[%s] Error indexing document ID=%d", res.Status(), i+1)
-			} else {
-				// Deserialize the response into a map.
-				var r map[string]interface{}
-				if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
-					log.Printf("Error parsing the response body: %s", err)
-				}
-			}
-
-			return nil
+			// TODO: improve error handling within goroutines
+			// https://www.atatus.com/blog/goroutines-error-handling/
+			indexDocument(es, u.ID.Hex(), u)
 		}(i, user)
 	}
-	wg.Wait()
 
+	wg.Wait()
 	log.Printf("Data successfully imported to ElasticSearch")
+
+	return nil
+}
+
+func indexDocument(es *elasticsearch.Client, ID string, user *User) error {
+	// Build the request body.
+	body, _ := json.Marshal(user)
+
+	// Set up the request object.
+	req := esapi.IndexRequest{
+		Index:      "users",
+		DocumentID: user.ID.Hex(),
+		Body:       bytes.NewReader(body),
+		Refresh:    "true",
+	}
+
+	// Perform the request with the client.
+	res, err := req.Do(context.Background(), es)
+	if err != nil {
+		return fmt.Errorf("Error getting response: %s", err)
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		return fmt.Errorf("[%s] Error indexing document ID=%s", res.Status(), ID)
+	}
+
+	// Deserialize the response into a map.
+	var r map[string]interface{}
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		return fmt.Errorf("Error parsing the response body: %s", err)
+	}
 
 	return nil
 }
