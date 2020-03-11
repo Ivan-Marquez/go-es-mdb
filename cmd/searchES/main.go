866ad4f0146a2771357d8e450e2f787678edc088
Ivan Marquez <js.ivan.marquez@gmail.com>
2020-03-11 19:53:51 -0400

    REF: searchES executable refactor
    refactor based on latest changes to all packages.

--- cmd/searchES/main.go
+++ cmd/searchES/main.go
@@ -5,24 +5,35 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/ivan-marquez/es-mdb/pkg/search"
-	"github.com/ivan-marquez/es-mdb/pkg/server"
+	"github.com/ivan-marquez/es-mdb/pkg/domain"
+	"github.com/ivan-marquez/es-mdb/pkg/http/rest"
 	"github.com/ivan-marquez/es-mdb/pkg/storage"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	logger := log.New(os.Stdout, "searchES ", log.LstdFlags|log.Lshortfile)
-	store, err := storage.New()
+	store, err := storage.NewStorage()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	h := search.NewHandlers(logger, store.ESClient)
+	var us domain.UserService
+	us = domain.NewUserService(store)
+	mw := rest.NewMiddleware(logger)
+	h := rest.NewHandler(mw, us)
 
 	mux := http.NewServeMux()
 	h.SetupRoutes(mux)
 
-	srv := server.New(mux, ":8080")
+	srv := rest.NewServer(mux, ":8080")
 
 	logger.Println("server starting")
 	err = srv.ListenAndServe()
