e3fe07e3989252e72aee2434bdebc888a96f2b01
Ivan Marquez <js.ivan.marquez@gmail.com>
2020-03-12 12:10:01 -0400

    IMP: search HTTP handler implementation

--- pkg/http/rest/handlers.go
+++ pkg/http/rest/handlers.go
@@ -1,6 +1,8 @@
 package rest
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/ivan-marquez/es-mdb/pkg/domain"
@@ -14,7 +16,33 @@ type Handler struct {
 
 // Search handles a request to retrieve users by term
 func (h *Handler) Search(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
+	keys, ok := r.URL.Query()["term"]
+
+	if !ok || len(keys[0]) < 1 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Bad request. Missing term parameter")
+		return
+	}
+
+	term := keys[0]
+
+	switch r.Method {
+	case http.MethodGet:
+		users, err := h.us.GetByTerm(term)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+
+		w.WriteHeader(http.StatusOK)
+		encoder := json.NewEncoder(w)
+		encoder.Encode(&users)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Bad request")
+	}
 }
 
 // SetupRoutes registers handles for API endpoints
